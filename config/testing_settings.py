5abfcb304dbfe1bd4dcfd16df89acacc78512251
watnalldt <davidaturner56@yahoo.com>
2024-03-14 17:28:37 +0000

    Configured recaptcha for testing

--- config/testing_settings.py
+++ config/testing_settings.py
@@ -2,3 +2,5 @@ from .settings import *  # noqa
 
 # An in-memory database should be good enough for now.
 DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}}
+RECAPTCHA_PUBLIC_KEY = '76wtgdfsjhsydt7r5FFGFhgsdfytd656sad75fgh'
+RECAPTCHA_PRIVATE_KEY = '98dfg6df7g56df6gdfgdfg65JHJH656565GFGFGs'
