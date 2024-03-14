cb7064eeca13d901ea0bf61601d773723d609457
watnalldt <davidaturner56@yahoo.com>
2024-03-14 19:12:17 +0000

    Amended test settings

--- config/testing_settings.py
+++ config/testing_settings.py
@@ -2,3 +2,8 @@ from .settings import *  # noqa
 
 # An in-memory database should be good enough for now.
 DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}}
+
+# Make sure that tests are never sending real emails.
+EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
+RECAPTCHA_PUBLIC_KEY = '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'
+RECAPTCHA_PRIVATE_KEY = '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'
