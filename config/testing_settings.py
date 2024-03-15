bb95913970053faae86ff44a8a21f833fe04810b
watnalldt <davidaturner56@yahoo.com>
2024-03-15 08:14:01 +0000

    Adjust test settings

--- config/testing_settings.py
+++ config/testing_settings.py
@@ -1,6 +1,6 @@
 from .settings import *  # noqa
 
-# An in-memory database should be good enough for now.
+
 
 # Make sure that tests are never sending real emails.
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
