505f8bd60a5d6d3685a172ebe1f748d273bf7fee
watnalldt <davidaturner56@yahoo.com>
2024-03-15 05:50:52 +0000

    Revise test settings

--- config/testing_settings.py
+++ config/testing_settings.py
@@ -1,7 +1,6 @@
 from .settings import *  # noqa
 
 # An in-memory database should be good enough for now.
-DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}}
 
 # Make sure that tests are never sending real emails.
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
